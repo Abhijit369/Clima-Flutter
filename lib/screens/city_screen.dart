bd145a1ef5fe6c5e0f02719f61b7105b3369e6dd
Abhijit369 <abhijitrath369@gmail.com>
2022-11-17 10:49:25 +0530

    error

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -3,7 +3,7 @@ import 'package:clima/utilities/constants.dart';
 
 class CityScreen extends StatefulWidget {
   @override
-  _CityScreenState createState() => _CityScreenState();
+  State<CityScreen> createState() => _CityScreenState();
 }
 
 class _CityScreenState extends State<CityScreen> {
@@ -23,7 +23,7 @@ class _CityScreenState extends State<CityScreen> {
             children: <Widget>[
               Align(
                 alignment: Alignment.topLeft,
-                child: FlatButton(
+                child: TextButton(
                   onPressed: () {},
                   child: Icon(
                     Icons.arrow_back_ios,
@@ -35,7 +35,7 @@ class _CityScreenState extends State<CityScreen> {
                 padding: EdgeInsets.all(20.0),
                 child: null,
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {},
                 child: Text(
                   'Get Weather',
